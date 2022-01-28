4d9d388b7ae949bda33452452ca81349a3ebbe8d
Samuel Sieberg <ssieberg@exsolid.com>
2022-01-28 06:24:25 +0100

    Added sound settings

--- Project-II/Assets/!Script/Misc/PlayerPrefKeys.cs
+++ Project-II/Assets/!Script/Misc/PlayerPrefKeys.cs
@@ -7,4 +7,6 @@ public static class PlayerPrefKeys
     private static string keyword = "Prj2_";
     public static readonly string JSON_CONTROLS = keyword + "Controls";
     public static readonly string MOUSE_SENSITIVITY = keyword + "Mouse_Sensitivity";
+    public static readonly string SOUND_VOLUME = keyword + "Sound_Volume";
+    public static readonly string MUSIC_VOLUME = keyword + "Music_Volume";
 }
